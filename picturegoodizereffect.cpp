93eb14bf6cf4cb1bf0abd145b3a8f091bfaab575
boudot <bastien.oudot@inria.fr>
2017-10-11 16:52:35 +0200

    Expanding PictureGoodizer effect to support image sequence

--- picturegoodizereffect.cpp
+++ picturegoodizereffect.cpp
@@ -26,92 +26,96 @@ void PictureGoodizerEffect::process()
     float velocity = widget->velocity;
 
     PureImage* data = static_cast<PureImage*>(PureCore::currentData);
-    QImage& img = data->getImage(0);
-    const int width = img.width();
-    const int height = img.height();
-
-    QMap<int, QVector<QPoint>> pixels;
-    pixels.clear();
-
-    for(int i = 0 ; i < width ; ++i)
+    for(int nbimg = 0 ; nbimg < data->getImageCount() ; ++nbimg)
     {
-        for(int j = 0 ; j < height ; ++j)
+        QImage& img = data->getImage(nbimg);
+        const int width = img.width();
+        const int height = img.height();
+
+        QMap<int, QVector<QPoint>> pixels;
+        pixels.clear();
+
+        for(int i = 0 ; i < width ; ++i)
         {
-            QColor c = img.pixelColor(i, j);
-            int key = c.value();
-            if(pixels.keys().contains(key))
+            for(int j = 0 ; j < height ; ++j)
             {
-                pixels[key].push_back(QPoint(i, j));
-            }
-            else
-            {
-                QVector<QPoint> p;
-                p.clear();
-                p.push_back(QPoint(i, j));
-                pixels.insert(key, p);
+                QColor c = img.pixelColor(i, j);
+                int key = c.value();
+                if(pixels.keys().contains(key))
+                {
+                    pixels[key].push_back(QPoint(i, j));
+                }
+                else
+                {
+                    QVector<QPoint> p;
+                    p.clear();
+                    p.push_back(QPoint(i, j));
+                    pixels.insert(key, p);
+                }
             }
         }
-    }
 
-    int totalPx = width * height;
+        int totalPx = width * height;
 
-    //how many low and high px we have to take
-    const int nbPx = ((static_cast<float>(totalPx)*static_cast<float>(power))/100.0f)/2;
-    qDebug()<<"nb px : "<<totalPx;
-    qDebug()<<"percent : "<<nbPx;
-    int cx = 0;
-    //find the lowest pixels
-    auto keys = pixels.keys();
-    for(int i = 0 ; i < keys.count() && cx < nbPx; ++i)
-    {
-        QVector<QPoint> pixel = pixels[keys[i]];
-        for(int j = 0 ; j < pixel.count() && cx < nbPx ; ++j)
+        //how many low and high px we have to take
+        const int nbPx = ((static_cast<float>(totalPx)*static_cast<float>(power))/100.0f)/2;
+        qDebug()<<"nb px : "<<totalPx;
+        qDebug()<<"percent : "<<nbPx;
+        int cx = 0;
+        //find the lowest pixels
+        auto keys = pixels.keys();
+        for(int i = 0 ; i < keys.count() && cx < nbPx; ++i)
         {
-
-            QColor c = img.pixelColor(pixel.at(j)).toHsv();
-
-            int saturation = static_cast<float>(c.saturation()) * velocity;
-            if(saturation > 255)
+            QVector<QPoint> pixel = pixels[keys[i]];
+            for(int j = 0 ; j < pixel.count() && cx < nbPx ; ++j)
             {
-                saturation = 255;
+
+                QColor c = img.pixelColor(pixel.at(j)).toHsv();
+
+                int saturation = static_cast<float>(c.saturation()) * velocity;
+                if(saturation > 255)
+                {
+                    saturation = 255;
+                }
+                int value = static_cast<float>(c.value())*velocity;
+                if(value > 255)
+                {
+                    value = 255;
+                }
+                c.setHsv(c.hue(),
+                         saturation,
+                         value);
+                img.setPixelColor(pixel.at(j), c);
+                ++cx;
             }
-            int value = static_cast<float>(c.value())*velocity;
-            if(value > 255)
-            {
-                value = 255;
-            }
-            c.setHsv(c.hue(),
-                     saturation,
-                     value);
-            img.setPixelColor(pixel.at(j), c);
-            ++cx;
         }
-    }
 
-    cx = 0;
-    //find the highest pixels
-    for(int i = keys.count() -1 ; i >= 0 && cx < nbPx ; --i)
-    {
-        QVector<QPoint> pixel = pixels[keys[i]];
-        for(int j = 0 ; j < pixel.count() && cx < nbPx ; ++j)
+        cx = 0;
+        //find the highest pixels
+        for(int i = keys.count() -1 ; i >= 0 && cx < nbPx ; --i)
         {
-            QColor c = img.pixelColor(pixel.at(j)).toHsv();
-            int saturation = static_cast<float>(c.saturation()) * velocity;
-            if(saturation > 255)
+            QVector<QPoint> pixel = pixels[keys[i]];
+            for(int j = 0 ; j < pixel.count() && cx < nbPx ; ++j)
             {
-                saturation = 255;
+                QColor c = img.pixelColor(pixel.at(j)).toHsv();
+                int saturation = static_cast<float>(c.saturation()) * velocity;
+                if(saturation > 255)
+                {
+                    saturation = 255;
+                }
+                int value = static_cast<float>(c.value())*velocity;
+                if(value > 255)
+                {
+                    value = 255;
+                }
+                c.setHsv(c.hue(),
+                         saturation,
+                         value);
+                img.setPixelColor(pixel.at(j), c);
+                ++cx;
             }
-            int value = static_cast<float>(c.value())*velocity;
-            if(value > 255)
-            {
-                value = 255;
-            }
-            c.setHsv(c.hue(),
-                     saturation,
-                     value);
-            img.setPixelColor(pixel.at(j), c);
-            ++cx;
         }
+
     }
 
 }
